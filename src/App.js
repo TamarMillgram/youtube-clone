350ed6c03c83180f6e10396dbe9e75bf7c14ed08
Etai Levi <etai3000@gmail.com>
2023-06-20 17:46:46 +0300

    UPDATE: Deleted comments from app.js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
 import { Route, HashRouter as Router, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import './assets/scss/main.scss';
 
-// import { store } from './store/store';
 import { AppHeader } from './cmps/app-header';
 import { Home } from './views/home';
-// import { AppFooter } from './cmps/app-footer';
-
-
 
 function App() {
   return (
-    // <Provider store={store}>
     <Router>
       <section className="main-layout app">
         <AppHeader />
@@ -21,10 +15,8 @@ function App() {
             <Route element={<Home />} path="/" />
           </Routes>
         </main>
-        {/* <AppFooter /> */}
       </section>
     </Router>
-    // </Provider>
   )
 }
 
