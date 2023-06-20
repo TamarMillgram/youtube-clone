e76772af07fc4a7f893c587949f3473d1e9d0e97
Etai Levi <etai3000@gmail.com>
2023-06-20 14:05:09 +0300

    UPDATE: Downloaded sass library

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './assets/scss/main.scss';
 
-import { store } from './store/store';
+// import { store } from './store/store';
 import { AppHeader } from './cmps/app-header';
-import { Home }from './views/home';
+import { Home } from './views/home';
 import { AppFooter } from './cmps/app-footer';
 
 
 
- function App() {
+function App() {
   return (
-    <Provider store={store}>
+    // <Provider store={store}>
+    <Provider>
       <Router>
         <section className="main-layout app">
           <AppHeader />
